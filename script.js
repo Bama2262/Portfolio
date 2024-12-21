24a36d3419b05a1fb1478675d5acc5dcc077138b
= <=>
2024-12-21 13:39:08 +0700

    Portfolio

--- script.js
+++
@@ -1,5 +0,0 @@
-window.onload = function() {
-    const audio = document.getElementById('backgroundMusic');
-    window.addEventListener('click', () => audio.play().catch(error => console.log('Autoplay error:', error)));
-    window.addEventListener('scroll', () => audio.play().catch(error => console.log('Autoplay error:', error)));
-}
